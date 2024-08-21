d525146d7236b5f69123d33edbbd0eecbe275195
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-08-21 14:48:00 +0530

    take pull from home to add common padding class

--- src/components/home/Cards.tsx
+++ src/components/home/Cards.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Cards = () => {
   return (
-    <div>Cards</div>
+    <div className='container'>
+      <div className='row'>
+        <div className='lg:w-1/3 px-3'></div>
+      </div>
+    </div>
   )
 }
 
