24420b830b0ebec32a467f4fb1dd4f12e2f1b29f
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-08-22 11:52:40 +0530

    use map in footer

---
+++ src/components/common/Helper.jsx
@@ -0,0 +1,16 @@
+import { FarbookLogo, InstagramLogo, LinkedinLogo } from './Icon'
+
+export const FOOTER_LINKS = [
+    {
+        title: 'Quick Links',
+        links: ['Services', 'About Us', 'Features', 'Plans'],
+    },
+    {
+        title: 'Support',
+        links: ['Help Center', 'Term & Conditions', 'Privacy Policy'],
+    },
+    // {
+    //     title: 'Follow Us',
+    //     links: [FacebookLogo, InstagramLogo, LinkedinLogo]
+    // }
+];
