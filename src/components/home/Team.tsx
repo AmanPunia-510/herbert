876d425f5c23853f8377ce659a45fc0f90c8167f
AmanPunia-510 <puniaaman888@gmail.com>
2024-08-22 15:49:49 +0530

    work on team sec

--- src/components/home/Team.tsx
+++ src/components/home/Team.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Heading from "../common/Heading";
 
 const Team = () => {
   return (
     <div className="common_padding">
       <div className="container">
         <div className="row relative">
-          <div className="col-5"></div>
+          <div className="col-5">
+          </div>
           <div className="col-7"></div>
         </div>
       </div>
