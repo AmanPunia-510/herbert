60f4f443e557a6cad1ad0ea64f3c8434ee18445b
AmanPunia-510 <puniaaman888@gmail.com>
2024-08-23 11:54:28 +0530

    make new folder of homehero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Cards from "@/components/home/Cards";
 import Faq from "@/components/home/Faq";
 import Features from "@/components/home/Features";
+import HomeHero from "@/components/home/HomeHero";
 import Plans from "@/components/home/Plans";
 import Team from "@/components/home/Team";
 
 export default function Home() {
   return (
     <>
+      <HomeHero />
       <Cards />
       <Features />
       <Plans />
