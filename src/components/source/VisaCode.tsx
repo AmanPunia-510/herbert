9a869351c74c2082b56bc56ba209528a070d835a
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-08-23 10:42:58 +0530

    start visacode section

--- src/components/source/VisaCode.tsx
+++ src/components/source/VisaCode.tsx
@@ -1,8 +1,29 @@
+import Image from 'next/image'
 import React from 'react'
 
 const VisaCode = () => {
   return (
-    <div>VisaCode</div>
+    <div className='bg-orange-30 common_padding'>
+      <div className='container'>
+        <div className='row'>
+          <div className='lg:w-1/2 px-3'>
+            <div className='row'>
+              <div className='w-1/2 px-3'>
+                <div className="bg-white max-w-[248px] w-full h-[188px] flex justify-center items-center rounded-lg">
+                  <Image
+                    src='/assets/images/sources/png/visa-application-img.png'
+                    width={72}
+                    height={72}
+                    alt='application'
+                  />
+                  <h4 className='font-medium text-xl leading-6 text-center max-w-[197px] pt-4'>Visa application processes</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
